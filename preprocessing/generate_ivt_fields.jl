33cf408c9273ccaa6cb92fc0ee36455e6fc4582c
Denis <yum-yab-dev@pm.me>
2024-02-21 18:38:49 +0100

    adds correct hybrid sigma pressure calc
    Also some nice acces to data over bounds

--- preprocessing/generate_ivt_fields.jl
+++ preprocessing/generate_ivt_fields.jl
@@ -15,52 +15,41 @@ module preprocessing
   struct GeographicBounds
     """This struct sets the geographic boundaries of the datasets. """
     lon_bounds::Tuple{<:Real, <:Real}
+    lon_indices::Tuple{<:Int, <:Int}
+    remaining_lon_values::Vector{<:Real}
     lat_bounds::Tuple{<:Real, <:Real}
+    lat_indices::Tuple{<:Int, <:Int}
+    remaining_lat_values::Vector{<:Real}
+  end
+
+  function GeographicBounds(lon_bounds::Tuple{<:Real, <:Real}, lat_bounds::Tuple{<:Real, <:Real}, lon_values::Vector{<:Real}, lat_values::Vector{<:Real})
+  
+    first_lon_id = findfirst(x -> x >= lon_bounds[1], lon_values)
+    second_lon_id = findlast(x -> x <= lon_bounds[2], lon_values)
+
+    first_lat_id = findfirst(x -> x >= lat_bounds[1], lat_values)
+    second_lat_id = findlast(x -> x <= lat_bounds[2], lat_values)
+    
+    function get_overflowing_result(bounds, values)
+      if bounds[1] < bounds[2]
+        remaining_values = values[bounds[1]:bounds[2]]
+      else
+        remaining_values = vcat(values[bounds[1]:end], values[1:bounds[2]]) 
+      end
+      return remaining_values
+    end
+    
+    return GeographicBounds(lon_bounds, (first_lon_id, second_lon_id), get_overflowing_result(lon_bounds, lon_values), lat_bounds, (first_lat_id, second_lat_id), get_overflowing_result(lat_bounds, lat_values))
+  end
+
+  function GeographicBounds(lon_bounds::Tuple{<:Real, <:Real}, lat_bounds::Tuple{<:Real, <:Real}, dataset, lon_id = :lon, lat_id = :lat)
+    lon_vals = dataset[lon_id][:]
+    lat_vals = dataset[lat_id][:]
+
+    return GeographicBounds(lon_bounds, lat_bounds, lon_vals, lat_vals)
   end
   
 
-  function get_range_of_bounds(geo_bnds::GeographicBounds, values::Vector{<:Real}, bound_type::Symbol)
-    if bound_type == :lon
-      bounds = geo_bnds.lon_bounds
-    elseif bound_type == :lat
-      bounds = geo_bnds.lat_bounds
-    else
-      throw(ErrorException("Can't use symbol $bound_type, please use :lon or :lat"))
-    end
-    
-    if bounds[1] < bounds[2]
-      first_id = findfirst(x -> x >= bounds[1], values)
-      last_id = findlast(x -> x <= bounds[2], values)
-      return first_id:last_id
-    else
-      
-      return values[(values .>= bounds[1]) .| (values .<= bounds[2])]  
-    end
-  end
-  
-
-  """
-      prepare_merged_file(input_files::Vector{String}, output_file::String)::String
-  
-  Merges input files, cuts out lon and lat bounds and interpolates the hybrid sigma pressure levels to the configured pressure levels. Result is written to output file.
-  """
-  function prepare_merged_file(input_files::Vector{String}, output_file::String, pressure_levels::Vector{Int} = round.(Int, LinRange(100000, 0, 47)), cdo_options = [])::String
-  
-    LON_BOUNDS=(270, 40)
-    LAT_BOUNDS=(20, 80)
-    
-    cdo_command_args = ["cdo", cdo_options..., "-ml2pl,$(join(string.(pressure_levels), ","))", "-sellonlatbox,$(LON_BOUNDS[1]),$(LON_BOUNDS[2]),$(LAT_BOUNDS[1]),$(LAT_BOUNDS[2])", "-selvar,hus,ua,va", "-merge", input_files..., output_file]
-    
-    cdo_cmd = Cmd(cdo_command_args)
-    
-    return run(cdo_cmd, wait=true)
-  end
-  
-  function nc_files_in_directory(directory_path::String)::Vector{String}
-  
-    return filter(path -> endswith(path, ".nc"), readdir(directory_path, join = true))
-  end
-  
   
   function find_common_versions(member_path::String, field_ids::Vector{String}, time_res_id::String)::Union{String, Nothing}
   
@@ -82,26 +71,65 @@ module preprocessing
     return split(split(nc_file_name, "_")[end], ".")[1]
   end
   
-  
-  
-  
-  function generate_ivt_field(merged_dataset_path::String, output_path::String)::Nothing
-  
-    merged_dataset = NCDataset(merged_dataset_path)
-  
-    lon_size = size(merged_dataset[:lon], 1)
-  
-    time_size = size(merged_dataset[:time], 1)
+"""This function loads data in given geographic bounds. It supports loading values going 'over the end' like the lon rage from 270-40 NOTE: It is expected that the longitude is given in values from 0-360 deg and lat in range from -90:90""" 
+function load_data_in_geo_bounds(dataset, field_id::Union{String, Symbol, Missing}, geo_bnds::GeographicBounds, indices...)::Array
+
+  lon_normal_range = geo_bnds.lon_bounds[1] < geo_bnds.lon_bounds[2]
+  lat_normal_range = geo_bnds.lat_bounds[1] < geo_bnds.lat_bounds[2]
+
+
+  if lon_normal_range & lat_normal_range
+    return dataset[field_id][geo_bnds.lon_indices[1]:geo_bnds.lon_indices[2], geo_bnds.lat_indices[1]:geo_bnds.lat_indices[2], indices...]
+  elseif !lon_normal_range & lat_normal_range
     
-    lat_size = size(merged_dataset[:lat], 1)
+    lon_first = dataset[field_id][geo_bnds.lon_indices[1]:end, geo_bnds.lat_indices[1]:geo_bnds.lat_indices[2],indices...]
+    lon_second = dataset[field_id][1:geo_bnds.lon_indices[2], geo_bnds.lat_indices[1]:geo_bnds.lat_indices[2],indices...]
+    
+    return vcat(lon_first, lon_second)
+  elseif lon_normal_range & !lat_normal_range
+    
+    lat_first = dataset[field_id][geo_bnds.lon_bounds[1]:geo_bnds.lon_indices[2], geo_bnds.lat_indices[1]:end,indices...]
+    lat_second = dataset[field_id][geo_bnds.lon_bounds[1]:geo_bnds.lon_indices[2], 1:geo_bnds.lat_indices[2],indices...]
+    
+    return hcat(lat_first, lat_second)
+  else
+    # last case is both are over 
+    lon_f_lat_f = dataset[field_id][geo_bnds.lon_bounds[1]:end, geo_bnds.lat_indices[1]:end,indices...]
+    lon_f_lat_s = dataset[field_id][geo_bnds.lon_bounds[1]:end, 1:geo_bnds.lat_indices[2],indices...]
+    lon_first = cat(lon_f_lat_f, lon_f_lat_s; dims = 2)
+
+    lon_s_lat_f = dataset[field_id][1:geo_bnds.lon_indices[2], geo_bnds.lat_indices[1]:end,indices...]
+    lon_s_lat_s = dataset[field_id][1:geo_bnds.lon_indices[2], 1:geo_bnds.lat_indices[2],indices...]
+
+    return vcat(hcat(lon_f_lat_f, lon_f_lat_s), hcat(lon_s_lat_f, lon_s_lat_s))
+  end
+end
   
-    levs = merged_dataset[:lev][:]
-    hus_data = merged_dataset[:hus][:, :, :, :]
-    ua_data = merged_dataset[:ua][:, :, :, :]
-    va_data = merged_dataset[:va][:, :, :, :]
-    ps_data = merged_dataset[:ps][:, :, :]
+
+  function generate_ivt_field(relevant_fields::Union{Vector{String}, String}, output_path::String, lon_bounds::Tuple{<:Real, <:Real}, lat_bounds::Tuple{<:Real, <:Real})::Nothing
+    if typeof(relevant_fields) == String
+      dataset = NCDataset(relevant_fields)
+    else
+      dataset = NCDataset(relevant_fields, aggdim = "")
+    end
   
-    result_data::Array{Union{Float32, Missing}, 3} = zeros(Float32, lon_size, lat_size, time_size)
+    
+    geo_bnds = GeographicBounds(lon_bounds, lat_bounds, dataset)
+
+    hus_data = load_data_in_geo_bounds(dataset, :hus, geo_bnds, :, :)
+    ua_data = load_data_in_geo_bounds(dataset, :ua, geo_bnds, :, :)
+    va_data = load_data_in_geo_bounds(dataset, :va, geo_bnds, :, :)
+    ps_data = load_data_in_geo_bounds(dataset, :ps, geo_bnds, :)
+    
+    lon_size = size(hus_data, 1)
+    lat_size = size(hus_data, 2)
+
+    # these variables are used for calculation of pressure levels at each specific lat, lon, time coordinate: p = ap + b * ps
+    ap = dataset[:ap][:]
+    b = dataset[:b][:]
+    time_size = size(dataset[:time], 1)
+
+    result_data::Array{Union{Float64, Missing}, 3} = zeros(lon_size, lat_size, time_size)
     
     Threads.@threads for time in 1:time_size
       for lat in 1:lat_size
@@ -111,18 +139,20 @@ module preprocessing
           ua_column::Vector{Union{Float32, Missing}} = ua_data[lon, lat, :, time]
           va_column::Vector{Union{Float32, Missing}} = va_data[lon, lat, :, time]
 
-          ps::Float32 = ps_data[lon, lat, time]
+          ps = ps_data[lon, lat, time]
+          pressure_levels = ap + b * ps
   
-          vertical_column_data = VerticalColumnData(hus_column, ua_column, va_column, ps)
+          vertical_column_data = VerticalColumnData(hus_column, ua_column, va_column, pressure_levels, ps)
   
-          result_data[lon, lat, time] = ivt_of_column(vertical_column_data, levs)
+          result_data[lon, lat, time] = ivt_of_column(vertical_column_data)
   
         end
       end
     end
+
     new_attrib = OrderedDict()
 
-    for (k, v) in merged_dataset.attrib
+    for (k, v) in dataset.attrib
       new_attrib[k] = v 
     end
         
@@ -133,18 +163,29 @@ module preprocessing
   
     NCDataset(output_path, "c", attrib = new_attrib) do ds
   
+      # defVar(ds, "lon", geo_bnds.remaining_lon_values, ("lon"), attrib = OrderedDict(
+      #   "units" => "degrees east",
+      #   "standard_name" => "longitude"
+      # ))
+      # defVar(ds, "lat", geo_bnds.remaining_lat_values, ("lat"), attrib = OrderedDict(
+      #   "units" => "degrees north",
+      #   "standard_name" => "latitude"
+      # ))
+      # defVar(ds, "time", dataset[:time][:], ("time"), attrib = OrderedDict(
+      #   "units" => "days since 1850-1-1 00:00:00",
+      #   "standard_name" => "time"
+      # ))
       defVar(ds, "ivt", result_data, ("lon", "lat", "time"), attrib = OrderedDict(
         "units" => "kg/ms",
         "comments" => "This field is NOT normalized over time or temperature or whatever"
       ))
       
+      
     end
   
-    
+    close(dataset)
   
-    close(merged_dataset)
-  
-    
+    return 
   end
   
   
