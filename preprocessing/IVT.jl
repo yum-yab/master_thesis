10293b781b458835b6e3737781e6cc04dcaf2218
Denis <yum-yab-dev@pm.me>
2024-02-21 18:38:49 +0100

    adds correct hybrid sigma pressure calc
    Also some nice acces to data over bounds

--- preprocessing/IVT.jl
+++ preprocessing/IVT.jl
@@ -3,32 +3,33 @@ module IVT
   export ivt_of_column, VerticalColumnData
 
   struct VerticalColumnData
-  """Struct for one vertical column of atmospheric data. Containing the vertical column of specific humidity (hus), """
+  """Struct for one vertical column of atmospheric data. Containing the vertical column of specific humidity (hus), eastward_wind ()"""
     specific_humidity::Vector{Union{Float32, Missing}}
     eastward_wind::Vector{Union{Float32, Missing}}
     southward_wind::Vector{Union{Float32, Missing}}
+    pressure_levels::Vector{Float64}
     surface_pressure::Float32
   end
   
-  function ivt_of_column(column_data::VerticalColumnData, pressure_levels::Vector{Float64})::Union{Float32, Missing}
+  function ivt_of_column(column_data::VerticalColumnData)::Union{Float64, Missing}
 
-    nmax = size(pressure_levels, 1) + 1
+    nmax = size(column_data.pressure_levels, 1) + 1
     
     g = 9.806
 
     ps = column_data.surface_pressure
   
-    function ph(i::Int)::Float32
+    function ph(i::Int)
       if i == 1
         return 0 
       elseif i == nmax
         return ps
       else
-      return ((pressure_levels[i] * ps) + (pressure_levels[i-1] * ps))/2
+      return (column_data.pressure_levels[i] + column_data.pressure_levels[i-1])/2
       end
     end
   
-    function calculate_layer_values(i::Int)::Tuple{Union{Float32, Missing}, Union{Float32, Missing}}
+    function calculate_layer_values(i::Int)
       dp = ph(i + 1) - ph(i)
       dm = dp/g
       # now here we strive from the description and multiply it also with the wind component
@@ -36,7 +37,7 @@ module IVT
       va_dq = column_data.specific_humidity[i] * dm * column_data.southward_wind[i]
       ua_dq = column_data.specific_humidity[i] * dm * column_data.eastward_wind[i]
   
-      return va_dq, ua_dq
+      return ua_dq, va_dq
     end
   
     sum_va_hus = 0
@@ -44,7 +45,7 @@ module IVT
   
   
     for i in 1:nmax-1
-      (va_layer_value, ua_layer_value) = calculate_layer_values(i)
+      (ua_layer_value, va_layer_value) = calculate_layer_values(i)
       sum_va_hus += va_layer_value
       sum_ua_hus += ua_layer_value
     end
