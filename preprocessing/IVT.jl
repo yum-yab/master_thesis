ca016c3004565275c64c854ed820278db56b9146
Denis <yum-yab-dev@pm.me>
2024-02-27 15:12:10 +0100

    Adds some benchmarking implementations

--- preprocessing/IVT.jl
+++ preprocessing/IVT.jl
@@ -54,6 +54,87 @@ module IVT
     
     return sqrt(sum_ua_hus^2 + sum_va_hus^2)
   end
+
+  function ivt_of_column_vectors(ps::Float64, plevs::Vector{Float64}, hus_data::Vector{Union{Float32, Missing}}, ua_data::Vector{Union{Float32, Missing}}, va_data::Vector{Union{Float32, Missing}})::Union{Float64, Missing}
+
+    nmax = size(plevs, 1) + 1
+    
+    g = 9.806
+  
+    function ph(i::Int)::Float32
+      if i == 1
+        return 0 
+      elseif i == nmax
+        return ps
+      else
+      return (plevs[i] + plevs[i-1])/2
+      end
+    end
+  
+    function calculate_layer_values(i::Int)::Float32
+      dp = ph(i + 1) - ph(i)
+      dm = dp/g
+      # now here we strive from the description and multiply it also with the wind component
+      
+      va_dq = hus_data[i] * dm * va_data[i]
+      ua_dq = hus_data[i] * dm * ua_data[i]
+      
+      return ua_dq, va_dq
+    end
+  
+    sum_va_hus = 0.
+    sum_ua_hus = 0.
+  
+  
+    for i in 1:nmax-1
+      (ua_layer_value, va_layer_value) = calculate_layer_values(i)
+      sum_va_hus += va_layer_value
+      sum_ua_hus += ua_layer_value
+    end
+    
+    return sqrt(sum_ua_hus^2 + sum_va_hus^2)
+  end
+
+  function ivt_of_column_vectors(ps::Float64, data::Matrix{Union{Float32, Missing}})::Union{Float64, Missing}
+
+    # corresponds in order with VerticalColumnData
+    nmax = size(data[4, :], 1) + 1
+    
+    g = 9.806
+  
+    function ph(i::Int)::Float32
+      if i == 1
+        return 0 
+      elseif i == nmax
+        return ps
+      else
+      return (data[4, :] + data[i-1, :])/2
+      end
+    end
+  
+    function calculate_layer_values(i::Int)::Float32
+      dp = ph(i + 1) - ph(i)
+      dm = dp/g
+      # now here we strive from the description and multiply it also with the wind component
+      
+      va_dq = data[1, i] * dm * data[3, i]
+      ua_dq = data[1, i] * dm * data[2, i]
+      
+      return ua_dq, va_dq
+    end
+  
+    sum_va_hus = 0.
+    sum_ua_hus = 0.
+  
+  
+    for i in 1:nmax-1
+      (ua_layer_value, va_layer_value) = calculate_layer_values(i)
+      sum_va_hus += va_layer_value
+      sum_ua_hus += ua_layer_value
+    end
+    
+    return sqrt(sum_ua_hus^2 + sum_va_hus^2)
+  end
   
 end
 
