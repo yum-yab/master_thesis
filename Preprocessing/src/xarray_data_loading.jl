571802d487c29a8312c01e8f93c3a3c7c1a45bb3
Denis <yum-yab-dev@pm.me>
2024-03-27 16:17:13 +0100

    Some debuggin preint statements

--- Preprocessing/src/xarray_data_loading.jl
+++ Preprocessing/src/xarray_data_loading.jl
@@ -14,11 +14,14 @@ end
 
 function remap_and_load_data_with_xarray(files::Vector{String})::Dict{String, Array{<: AbstractFloat}}
   
+  println("Opening the dataset")
   ds = xr.open_mfdataset(files, chunks=py"dict(time=256, lev= 47, lat= 96, lon= 192)")
-
+  
+  println("Remapping longitude coordinates")
   ds.coords["lon"] = (ds.coords["lon"] + 180) % 360 - 180
   relevant_subset = ds.sortby(ds.lon).sel(lon=py"slice(-90, 40)", lat=py"slice(20, 80)")
-
+  
+  println("Actually loading the data")
   result_dict = Dict([id => relevant_subset[id].to_numpy() for id in ["ap", "b", "ps", "hus", "ua", "va"]])
 
   ds.close()
