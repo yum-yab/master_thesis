39f2c75fe4ec77f5a9e0b608a694c0f5e718ca90
Denis <yum-yab-dev@pm.me>
2024-03-28 19:56:17 +0100

    FIxes wrong selection bug in xarray

--- Preprocessing/src/xarray_data_loading.jl
+++ Preprocessing/src/xarray_data_loading.jl
@@ -20,15 +20,13 @@ function parallel_loading_of_datasets(id_to_file_mapping::Dict{String, String}):
 end
 function load_one_dataset_xarray(path::String, id::String)::AbstractArray{<: AbstractFloat}
     
-    println("Hello from worker $id")
+    println("Started loading data $id from $path")
     xr = pyimport("xarray")
     ds = xr.open_dataset(path, chunks=pydict(lon = 192, lat = 96, lev = 47, time= 256))
-  
-    ds.assign_coords(Dict("lon" => (ds.lon.values .+ 180) .% 360 .- 180))
+    ds.coords["lon"] = (ds.coords["lon"] + 180) % 360 - 180
     relevant_subset = ds.sortby(ds.lon).sel(lon=pyslice(-90, 40), lat=pyslice(20, 80))
     
-    println("Actually loading the data")
-    @time data = relevant_subset[id].to_numpy() 
+    data = relevant_subset[id].to_numpy() 
   
     ds.close()
   
