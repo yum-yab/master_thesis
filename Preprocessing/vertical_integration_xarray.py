3f650cd98696d02b272cf39888812eb9ca800710
Denis <yum-yab-dev@pm.me>
2024-04-03 13:50:34 +0200

    Adds timing for functions and probably accc opening

--- Preprocessing/vertical_integration_xarray.py
+++ Preprocessing/vertical_integration_xarray.py
@@ -1,6 +1,6 @@
 import xarray as xr 
 import numpy as np
-
+import time
 
 paths = ["/home/denis/Documents/Uni/Master/MA/preprocessing/sample_data/sample_hus_dataset_200_timesteps.nc", "/home/denis/Documents/Uni/Master/MA/preprocessing/sample_data/sample_va_dataset_200_timesteps.nc", "/home/denis/Documents/Uni/Master/MA/preprocessing/sample_data/sample_ua_dataset_200_timesteps.nc"]
 
@@ -22,8 +22,12 @@ def integrate_over_data_array(y_values, x_values, integration_dim = 'lev'):
         output_dtypes=[float]  # Specify the output data type
      )
     
+start_time = time.perf_counter()
 
-ds = xr.open_mfdataset(paths, compat="override", parallel=True, chunks=dict(time=256, lev= 47, lat= 96, lon= 192))
+ds = xr.open_mfdataset(paths, compat="override", parallel=True, decode_cf=False, chunks=dict(time=256, lev= 47, lat= 96, lon= 192))
+
+opened_datasets = time.perf_counter()
+print(f"Time used for loading dataset: {opened_datasets - start_time} s")
 
 ds.coords["lon"] = (ds.coords["lon"] + 180) % 360 - 180
     
@@ -49,4 +53,12 @@ new_ds["eastward_integral"] = eastward_integral
 new_ds["northward_integral"] = northward_integral
 new_ds["ivt"] = ivt_norm
 
+all_lazy_calcs = time.perf_counter()
+print(f"Time used for lazy calculations: {all_lazy_calcs - opened_datasets}")
+
 new_ds.to_netcdf(target_path)
+
+very_end = time.perf_counter()
+
+print(f"Time used for actual calculation: {very_end - all_lazy_calcs} s")
+print(f"Time used all in all: {(very_end - start_time)/60} min")
